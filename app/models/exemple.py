bcecae3435ac55842073ec40dd3ae1a04588e45a
EstimaSahuma <estimaetilenoglicol@gmail.com>
2023-08-03 16:03:52 +0100

    fix: somechanges

--- app/models/exemple.py
+++ app/models/exemple.py
@@ -5,5 +5,5 @@ class Example(Base):
     __tablename__ = "examples"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    description = Column(String)
+    name = Column(String(100), index=True)
+    description = Column(String(100))
